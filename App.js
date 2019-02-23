af7482ebdb402679a6b4734eca48804b83a00d38
safe <krishna.s@lucideustech.com>
2019-02-23 20:32:33 +0530

    adding a button and managing state

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default class App extends Component {
   state = {
-    placeName: ""
+    placeName: "",
+    places: []
   };
 
   placeNameChangedHandler = val => {
@@ -12,17 +13,35 @@ export default class App extends Component {
     });
   };
 
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      }
+    });
+
+  }
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <Text key={i}>{place}</Text>
+    ));
     return (
       <View style={styles.container}>
-        <View stye={styles.inputContainer}>
+        <View style={styles.inputContainer}>
           <TextInput
             style={styles.placeInput}
             placeholder="An awesome place"
             value={this.state.placeName}
             onChangeText={this.placeNameChangedHandler}
           />
-          <Button title="Add" style={styles.placeButton} />
+          <Button title="Add" style={styles.placeButton} onPress={this.placeSubmitHandler} />
+        </View>
+        <View>
+          {placesOutput}
         </View>
 
       </View>
