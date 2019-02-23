ba901278b4fc617b6dbffc36c1321efb3aa79780
safe <krishna.s@lucideustech.com>
2019-02-24 00:18:23 +0530

    create custom component

---
+++ src/ListItem/ListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ListItem = (props) => (
+    <View style={styles.listItem}>
+        <Text >{props.placeName}</Text>
+    </View>
+)
+
+
+const styles = StyleSheet.create({
+    listItem: {
+        width: "100%",
+        backgroundColor: "#eee",
+        padding: 10,
+        marginBottom: 5
+
+    }
+
+});
+
+export default ListItem;
+
