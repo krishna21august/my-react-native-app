f7fd9c62c545d390ebe75b929808a344c7452b53
safe <krishna.s@lucideustech.com>
2019-03-02 06:57:18 +0530

    add icons

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -3,23 +3,30 @@ import Icon from 'react-native-vector-icons/Ionicons'
 
 
 const startTabs = () => {
+    //both of these return promise. to deal with multiple promise and letting them resolve we use Promise.all //
+    Promise.all([
+        Icon.getImageSource("md-map", 30),
+        Icon.getImageSource("ios-share-alt", 30)
+    ]).then(sources => {
+        Navigation.startTabBasedApp({
+            tabs: [
+                {
+                    screen: "awesome-places.FindPlaceScreen",
+                    label: "Find Place",
+                    title: "Find Place",
+                    icon: sources[0]
+                },
+                {
+                    screen: "awesome-places.SharePlaceScreen",
+                    label: "Share Place",
+                    title: "Share Place",
+                    icon: sources[1]
+                }
+            ]
 
-    Navigation.startTabBasedApp({
-        tabs: [
-            {
-                screen: "awesome-places.FindPlaceScreen",
-                label: "Find Place",
-                title: "Find Place"
-            },
-            {
-                screen: "awesome-places.SharePlaceScreen",
-                label: "Share Place",
-                title: "Share Place"
-            }
-        ]
+        });
 
     });
-
 }
 
 export default startTabs;
