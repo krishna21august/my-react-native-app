14ebf4c03b391a0e71ba1a954ab8ffb5addd3ce5
safe <krishna.s@lucideustech.com>
2019-03-02 06:34:48 +0530

    add screen tabs and register components

---
+++ src/screens/SharePlace/SharePlace.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class SharePlaceScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>On share place screen</Text>
+            </View>
+        )
+
+    }
+
+
+}
+
+export default SharePlaceScreen;
