462b87afbf1b49a3e1e71ee0213c5184e220f07a
safe <krishna.s@lucideustech.com>
2019-03-03 10:59:27 +0530

    navbar and sidebars implemented

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
-import startMainTabs from '../MainTabs/startMainTabs'
+
+import startMainTabs from '../MainTabs/startMainTabs';
 
 class AuthScreen extends Component {
-
     loginHandler = () => {
         startMainTabs();
     }
 
-    render() {
+    render () {
         return (
             <View>
                 <Text>Auth Screen</Text>
-                <Button title="Login" onPress={this.loginHandler} />
+                <Button title="Login" onPress={this.loginHandler}/>
             </View>
         );
     }
--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,12 +1,11 @@
 import { Navigation } from 'react-native-navigation';
-import Icon from 'react-native-vector-icons/Ionicons'
-
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
-    //both of these return promise. to deal with multiple promise and letting them resolve we use Promise.all //
     Promise.all([
         Icon.getImageSource("md-map", 30),
-        Icon.getImageSource("ios-share-alt", 30)
+        Icon.getImageSource("ios-share-alt", 30),
+        Icon.getImageSource("ios-menu", 30)
     ]).then(sources => {
         Navigation.startTabBasedApp({
             tabs: [
@@ -14,20 +13,40 @@ const startTabs = () => {
                     screen: "awesome-places.FindPlaceScreen",
                     label: "Find Place",
                     title: "Find Place",
-                    icon: sources[0]
+                    icon: sources[0],
+                    navigatorButtons: {
+                        leftButtons: [
+                            {
+                                icon: sources[2],
+                                title: "Menu",
+                                id: "sideDrawerToggle"
+                            }
+                        ]
+                    }
                 },
                 {
                     screen: "awesome-places.SharePlaceScreen",
                     label: "Share Place",
                     title: "Share Place",
-                    icon: sources[1]
+                    icon: sources[1],
+                    navigatorButtons: {
+                        leftButtons: [
+                            {
+                                icon: sources[2],
+                                title: "Menu",
+                                id: "sideDrawerToggle"
+                            }
+                        ]
+                    }
                 }
-            ]
-
+            ],
+            drawer: {
+                left: {
+                    screen: "awesome-places.SideDrawer"
+                }
+            }
         });
-
     });
-}
+};
 
 export default startTabs;
-
