7d48a635024d178792d12530bb8c41574ef23094
safe <krishna.s@lucideustech.com>
2019-03-06 05:47:36 +0530

    input field done

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,39 +1,50 @@
-import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import React, { Component } from "react";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 
-import startMainTabs from '../MainTabs/startMainTabs';
+import startMainTabs from "../MainTabs/startMainTabs";
+import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
+import HeadingText from "../../components/UI/HeadingText/HeadingText";
+import MainText from "../../components/UI/MainText/MainText";
 
 class AuthScreen extends Component {
-    loginHandler = () => {
-        startMainTabs();
-    }
+  loginHandler = () => {
+    startMainTabs();
+  };
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Please Log In</Text>
-                <Button title="Switch to login" />
-                <View style={styles.inputContainer}>
-                    <DefaultInput placeholder="Enter Email" />
-                    <DefaultInput placeholder="Password" />
-                    <DefaultInput placeholder="Confirm Password" />
-                </View>
-                <Button title="Submit" onPress={this.loginHandler} />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <MainText>
+          <HeadingText>Please Log In</HeadingText>
+        </MainText>
+        <Button title="Switch to Login" />
+        <View style={styles.inputContainer}>
+          <DefaultInput
+            placeholder="Your E-Mail Address"
+            style={styles.input}
+          />
+          <DefaultInput placeholder="Password" style={styles.input} />
+          <DefaultInput placeholder="Confirm Password" style={styles.input} />
+        </View>
+        <Button title="Submit" onPress={this.loginHandler} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, //take full available space
-        justifyContent: "center",//alignment with main axis i.e y axis (column),
-        alignItems: "center" //alignment with cross axis
-    },
-    inputContainer: {
-        width: "80%"
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  input: {
+    backgroundColor: "#eee",
+    borderColor: "#bbb"
+  }
 });
 
 export default AuthScreen;
